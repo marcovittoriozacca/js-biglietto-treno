565b24b5d46225d026b6ef4b75150330bf80e149
marcovittoriozacca <zaccamarco01@gmail.com>
2023-12-22 22:00:38 +0100

    feat: style

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,45 +1,60 @@
+// Dichiarazione variabili e costanti
 let kmNumber, psgAge, fullPrice, discount20, discount40;
 const priceForkm = 0.21;
 
+// Funzione che permette di prendere i valori numerici dai campi di input
 function getValues(){
+
+    // Asseggnazione dei valori contenuti nei campi numerici alle variabili
     kmNumber = document.getElementById("numeroKM").value;
     psgAge = document.getElementById("etaPsg").value;
 
+    /* Controllo sui valori.
+    Se i campi sono vuoti o se i valori sono uguali o inferiori a zero, la funzione darà esito negativo */
     if ((kmNumber.length == 0 || psgAge.length == 0) || (kmNumber <= 0 || psgAge <= 0)){
-        document.getElementById("error").innerHTML = "Compila tutti i campi correttamente!";
-        document.getElementById("finalPrice").innerHTML = null;
+        // Cambio della classe d-none a d-flex per mostrare il messaggio di errore
+        let classFlex = document.querySelector("#classChange");
+        classFlex.classList.replace("d-none","d-flex");
+        document.getElementById("result").innerHTML = '<p class="text-danger">Compila tutti i campi correttamente!</p>';
     }
     else{
-        document.getElementById("error").innerHTML = null;
+        document.getElementById("result").innerHTML = null;
+        // Calcolo del prezzo pieno del biglietto
         fullPrice = kmNumber * priceForkm;
         fullPrice = fullPrice;
         
+        // Controllo sull'età inserità dall'utente per applicare eventuali sconti
         if(psgAge < 18){
             discount20 = fullPrice * 20 / 100;
             discount20 = fullPrice - discount20;
             discount20 = discount20.toFixed(2);
-            document.getElementById("finalPrice").innerHTML = `il prezzo del biglietto sarà di: ${discount20}€ <p>Al prezzo è stato applicato uno sconto del 20%! <p>`
+            let classFlex = document.querySelector("#classChange");
+            classFlex.classList.replace("d-none","d-flex");
+            document.getElementById("result").innerHTML = `<p>Prezzo biglietto: <span class="text-lime h5">${discount20}€</span></p><div class="opacity-50"><p>Al prezzo è stato applicato uno sconto del <span class="text-lime">20%</span></p><p>Prezzo originale: <span class="text-danger">${fullPrice.toFixed(2)}€</span></p>`
         }
         else if (psgAge >= 65){
             discount40 = fullPrice * 40 / 100;
             discount40 = fullPrice - discount40;
             discount40 = discount40.toFixed(2);
-            document.getElementById("finalPrice").innerHTML = `il prezzo del biglietto sarà di: ${discount40}€ <p>Al prezzo è stato applicato uno sconto del 40%! <p>`
+            let classFlex = document.querySelector("#classChange");
+            classFlex.classList.replace("d-none","d-flex");
+            document.getElementById("result").innerHTML = `<p>Prezzo biglietto: <span class="text-lime h5">${discount40}€</span></p><div class="opacity-50"><p>Al prezzo è stato applicato uno sconto del <span class="text-lime">40%</span></p><p>Prezzo originale: <span class="text-danger">${fullPrice.toFixed(2)}€</span></p>`
         }
         else{
             fullPrice = fullPrice.toFixed(2);
-            document.getElementById("finalPrice").innerHTML = `il prezzo del biglietto sarà di: ${fullPrice}€`
+            let classFlex = document.querySelector("#classChange");
+            classFlex.classList.replace("d-none","d-flex");
+            document.getElementById("result").innerHTML = `<p>Prezzo biglietto: <span class="text-lime h5">${fullPrice}€</span></p><p class="opacity-50">Purtroppo non è previsto nessuno sconto al momento!</p>`
         }
     }
-
-
 }
-
+// Resetta ogni elemento per riportare la pagina allo stato iniziale
 function resetAll(){
     kmNumber = document.getElementById("numeroKM").value = null;
     psgAge = document.getElementById("etaPsg").value = null;
-    document.getElementById("error").innerHTML = null;
-    document.getElementById("finalPrice").innerHTML = null;
+    document.getElementById("result").innerHTML = null;
+    let classFlex = document.querySelector("#classChange");
+    classFlex.classList.replace("d-flex","d-none");
 }
 
 // bottone per prendere i dati collegato alla funzione getValues()
